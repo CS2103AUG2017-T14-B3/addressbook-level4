90bba3f16d1535b896ef9988fb4b11f85f9037c4
huiyiiih <huiyih@HUIYIs-MacBook-Pro.local>
2017-10-07 02:20:45 +0800

    add removed trailing whitespaces

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,10 +21,10 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         try {
             return new Person[]{
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"), 
+                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                         new Address("Blk 30 Geylang Street 29, #06-40"), getTagSet("friends")),
-                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"), 
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), 
+                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                         getTagSet("colleagues", "friends")),
                 new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                         new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), getTagSet("neighbours")),
