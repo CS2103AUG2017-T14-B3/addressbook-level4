eb6c34f623043f59ae686389594187f24e60ce86
Junkai <reginleiff@gmail.com>
2017-11-13 17:40:14 +0800

    Organisation: Clean up Description test case

--- src/test/java/seedu/address/model/event/DescriptionTest.java
+++ src/test/java/seedu/address/model/event/DescriptionTest.java
@@ -1,4 +1,3 @@
-//@@author reginleiff
 package seedu.address.model.event;
 
 import static org.junit.Assert.assertFalse;
@@ -6,21 +5,30 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+//@@author reginleiff
 public class DescriptionTest {
+    private static final String INVALID_CASE_ONE = "";
+    private static final String INVALID_CASE_TWO = "  ";
+
+    private static final String VALID_CASE_ONE = "srgsrgffdh";
+    private static final String VALID_CASE_TWO = "12345";
+    private static final String VALID_CASE_THREE = "sasdgsdg898 s9898";
+    private static final String VALID_CASE_FOUR = "SGSDF839 SD928 92";
+    private static final String VALID_CASE_FIVE = "SDFIUSDFHIHI9839:2983983 2HIUDF938UN OJOFEIJ02JSLDLJDO90JS JOASDJ9";
+
+
 
     @Test
     public void isValidDescriptionTest() {
-        // invalid title
-        assertFalse(Description.isValidDescription("")); // empty string
-        assertFalse(Description.isValidDescription(" ")); // spaces only
+        // Invalid titles
+        assertFalse(Description.isValidDescription(INVALID_CASE_ONE)); // empty string
+        assertFalse(Description.isValidDescription(INVALID_CASE_TWO)); // spaces only
 
-        // valid title
-        assertTrue(Description.isValidDescription("srgsrgdfh")); // alphabets only
-        assertTrue(Description.isValidDescription("12345")); // numbers only
-        assertTrue(Description.isValidDescription("sasdgsdg898 s9898")); // alphanumeric characters
-        assertTrue(Description.isValidDescription("SGSDF839 SD928 92")); // with capital letters
-        // long descriptions with symbols
-        assertTrue(
-                Description.isValidDescription("SDFIUSDFHIHI9839:2983983 2HIUDF938UN OJOFEIJ02JSLDLJDO90JS JOASDJ9"));
+        // Valid titles
+        assertTrue(Description.isValidDescription(VALID_CASE_ONE)); // alphabets only
+        assertTrue(Description.isValidDescription(VALID_CASE_TWO)); // numbers only
+        assertTrue(Description.isValidDescription(VALID_CASE_THREE)); // alphanumeric characters
+        assertTrue(Description.isValidDescription(VALID_CASE_FOUR)); // with capital letters
+        assertTrue(Description.isValidDescription(VALID_CASE_FIVE)); // long strings with all of the above
     }
 }
